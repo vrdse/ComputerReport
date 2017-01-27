c89e33b3ca6b01bc1762e79112f5e457bdb03d0e
vrdse <vrdse@users.noreply.github.com>
2017-01-27 15:55:13 +0100

    optimization in getnetworklistener

--- Public/Get-NetworkListener.ps1
+++ Public/Get-NetworkListener.ps1
@@ -5,8 +5,9 @@ function Get-NetworkListener {
         [Microsoft.Management.Infrastructure.CimSession]
         $CimSession,
         [Parameter()]
-        [switch]
-        $TestAll
+        [ValidateSet(First1000Ports,First30000Ports,All)]
+        [string]
+        $TestConnection
     )
 
     $TCPConnections = Get-NetTCPConnection -State Listen -CimSession $CimSession | Select-Object -Property LocalPort, OwningProcess
@@ -14,20 +15,24 @@ function Get-NetworkListener {
 	Write-Verbose "ProcessIds are $Filter"
     $TCPProcesses = Get-CimInstance -ClassName Win32_Process -Filter $Filter -Property ProcessId,Name,CommandLine | Select-Object -Property ProcessId,Name,CommandLine
 
+    switch ($TestConnection) {
+        First1000Ports {$PortsToCheck = 1000}
+        First30000Ports {$PortsToCheck = 30000}
+        All {$PortsToCheck = 65535}
+        Default {$PortsToCheck = $null}
+    }
+
     foreach ($TCPConnection in $TCPConnections) {
 		Write-Verbose "Processing $($TCPConnection.LocalPort)"
-        if ($TestAll -ne $true) {
-            if ($TCPConnection.LocalPort -lt 30000) {
+        if ($PortsToCheck -ne $null) {
+            if ($TCPConnection.LocalPort -lt $PortsToCheck) {
                 $isReachable = Test-NetConnection -ComputerName $CimSession.ComputerName -Port $TCPConnection.LocalPort -InformationLevel Quiet
-            }
-            else {
-                $isReachable = $null
             }
         }
         else {
-            $isReachable = Test-NetConnection -ComputerName $CimSession.ComputerName -Port $TCPConnection.LocalPort -InformationLevel Quiet
+            $isReachable = $null
         }
-        $TCPProcess = $TCPProcesses | Where-Object {$PSItem.ProcessId -eq $TCPConnection.OwningProcess}
+        $TCPProcess = $TCPProcesses | Where-Object -Property ProcessId -EQ -Value $TCPConnection.OwningProcess
         [PSCustomObject]@{
             Port = $TCPConnection.LocalPort
             isReachable = $isReachable
