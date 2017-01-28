4f3db01f5b8c429bff0f914568c13dfc7bb02029
vrdse <vrdse@users.noreply.github.com>
2017-01-28 17:22:54 +0100

    fixes

--- Public/Get-NetworkListener.ps1
+++ Public/Get-NetworkListener.ps1
@@ -1,43 +1,64 @@
 function Get-NetworkListener {
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(ParameterSetName='CimSession')]
         [Microsoft.Management.Infrastructure.CimSession]
         $CimSession,
+        [Parameter(ParameterSetName='ComputerName')]
+        [string[]]
+        $ComputerName,
         [Parameter()]
-        [ValidateSet(First1000Ports,First30000Ports,All)]
+        [ValidateSet('First1000Ports', 'First30000Ports', 'All')]
         [string]
         $TestConnection
     )
 
-    $TCPConnections = Get-NetTCPConnection -State Listen -CimSession $CimSession | Select-Object -Property LocalPort, OwningProcess
-    $Filter = "ProcessId="+($($TCPConnections.OwningProcess) -join " or ProcessId=")
-	Write-Verbose "ProcessIds are $Filter"
-    $TCPProcesses = Get-CimInstance -ClassName Win32_Process -Filter $Filter -Property ProcessId,Name,CommandLine | Select-Object -Property ProcessId,Name,CommandLine
+    process {
+        $TCPConnections = Get-NetTCPConnection -State Listen -CimSession $CimSession | 
+                          Select-Object -Property LocalPort, OwningProcess
+        $Filter = "ProcessId="+($($TCPConnections.OwningProcess) -join " or ProcessId=")
+        Write-Verbose "ProcessIds are $Filter"
+        $TCPProcessesArguments = @{
+            ClassName   = 'Win32_Process'
+            Filter      = $Filter
+            Property    = 'ProcessId,Name,CommandLine'
+        }
+        $TCPProcesses = Get-CimInstance @TCPProcessesArguments | 
+                        Select-Object -Property ProcessId,Name,CommandLine
 
-    switch ($TestConnection) {
-        First1000Ports {$PortsToCheck = 1000}
-        First30000Ports {$PortsToCheck = 30000}
-        All {$PortsToCheck = 65535}
-        Default {$PortsToCheck = $null}
-    }
+        switch ($TestConnection) {
+            First1000Ports {$PortsToCheck = 1000}
+            First30000Ports {$PortsToCheck = 30000}
+            All {$PortsToCheck = 65535}
+            Default {$PortsToCheck = $null}
+        }
 
-    foreach ($TCPConnection in $TCPConnections) {
-		Write-Verbose "Processing $($TCPConnection.LocalPort)"
-        if ($PortsToCheck -ne $null) {
-            if ($TCPConnection.LocalPort -lt $PortsToCheck) {
-                $isReachable = Test-NetConnection -ComputerName $CimSession.ComputerName -Port $TCPConnection.LocalPort -InformationLevel Quiet
+        foreach ($TCPConnection in $TCPConnections) {
+            Write-Verbose "Processing $($TCPConnection.LocalPort)"
+            if ($PortsToCheck -ne $null) {
+                if ($TCPConnection.LocalPort -lt $PortsToCheck) {
+                    $TestNetConnectionArguments = @{
+                        ComputerName = $CimSession.ComputerName
+                        Port = $TCPConnection.LocalPort
+                        InformationLevel = 'Quiet'
+                    }
+                    $isReachable = Test-NetConnection @TestNetConnectionArguments
+                }
+
+            }
+            else {
+                $isReachable = $null
+            }
+
+            $TCPProcess = $TCPProcesses | 
+                          Where-Object -Property ProcessId -EQ -Value $TCPConnection.OwningProcess
+            [PSCustomObject]@{
+                Port = $TCPConnection.LocalPort
+                isReachable = $isReachable
+                ProcessName = $TCPProcess.Name
+                ProcessCmd = $TCPProcess.CommandLine
             }
         }
-        else {
-            $isReachable = $null
-        }
-        $TCPProcess = $TCPProcesses | Where-Object -Property ProcessId -EQ -Value $TCPConnection.OwningProcess
-        [PSCustomObject]@{
-            Port = $TCPConnection.LocalPort
-            isReachable = $isReachable
-            ProcessName = $TCPProcess.Name
-            ProcessCmd = $TCPProcess.CommandLine
-        }
+
     }
 }
