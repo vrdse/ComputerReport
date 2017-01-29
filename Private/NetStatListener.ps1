0502645ede1f0806f06e2100d9b7d9600dd94cdd
vrdse <vrdse@users.noreply.github.com>
2017-01-29 14:58:09 +0100

    netstat

--- Private/NetStatListener.ps1
+++ Private/NetStatListener.ps1
@@ -9,16 +9,29 @@ function Convertfrom-Netstat {
 
     $Input = $Input[4..$Input.Count]
     $InputObject = $Input | ConvertFrom-String -PropertyNames Empty, Protocol, LocalAddress, RemoteAddress, State, PID
-    $Listener = $InputObject | Where-Object -Property State -EQ -Value 'LISTEN'
+    #$Listener = $InputObject | Where-Object -Property State -EQ -Value 'LISTENING'
     $SelectArguments = @{
         Property = @{
+            Name = 'LocalAddress';
+            Expression = {((($PSItem.LocalAddress).Split(':'))[0..(($PSItem.LocalAddress).Split(':')).Count-1]) -join ':'}
+        },
+        @{
             Name = 'LocalPort';
             Expression = {(($PSItem.LocalAddress).Split(':'))[(($PSItem.LocalAddress).Split(':')).Count-1]}
         },
+        @{
+            Name = 'RemoteAddress';
+            Expression = {((($PSItem.RemoteAddress).Split(':'))[0..(($PSItem.RemoteAddress).Split(':')).Count-1]) -join ':'}
+        },
+        @{
+            Name = 'RemotePort';
+            Expression = {(($PSItem.RemoteAddress).Split(':'))[(($PSItem.RemoteAddress).Split(':')).Count-1]}
+        },       
+        'State',
         @{
             Name = 'OwningProcess';
             Expression = {$PSItem.PID}
         }
     }
-    $Listener | Select-Object @SelectArguments
+    $InputObject | Select-Object @SelectArguments
 }
